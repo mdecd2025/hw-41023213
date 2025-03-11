5aad6659cd0f52ab8e0969e26b0c8ee8b492f11d
41023213 <41023213@gm.nfu.edu.tw>
2025-03-11 11:48:54 +0800

    list更新 port8000連結

---
+++ downloads/cd2025_box_fourbar/cd2025_box_fourbar/controllers/fourbar_controller2/fourbar_controller2.py
@@ -0,0 +1,47 @@
+from controller import Robot, Keyboard
+
+def run_robot():
+    # Create the Robot instance
+    robot = Robot()
+    
+    # Get simulation time step
+    timestep = int(robot.getBasicTimeStep())
+    
+    # 初始化鍵盤，並設定取樣時間為 timestep
+    keyboard = Keyboard()
+    keyboard.enable(timestep)
+
+    # Get motor device
+    motor = robot.getDevice('motor2')
+
+    # Set motor for continuous rotation
+    motor.setPosition(float('inf'))
+    # 初始設定馬達速度為 0 (暫停狀態)
+    motor.setVelocity(0.0)
+    
+    # 設定一個變數來追踪馬達是否在運行
+    is_running = False
+    
+    print("模擬開始. 按下 's' 啟動馬達, 按下 'p' 暫停.")
+
+    # Main control loop
+    while robot.step(timestep) != -1:
+        # 取得鍵盤輸入
+        key = keyboard.getKey()
+        
+        # 將 key 轉換為對應的字符
+        key_char = chr(key).lower() if key != -1 else ''
+        
+        # 使用字符直接比較
+        if key_char == 's':
+            is_running = True
+            motor.setVelocity(1.0)
+            print("馬達啟動")
+            
+        elif key_char == 'p':
+            is_running = False
+            motor.setVelocity(0.0)
+            print("馬達暫停")
+
+if __name__ == "__main__":
+    run_robot()
---
+++ downloads/cd2025_box_fourbar/cd2025_box_fourbar/deg_to_rad.py
@@ -0,0 +1,7 @@
+import math
+
+deg = 2*math.pi/360.
+
+print(round(90*deg, 4))
+print(round(55.12*deg, 4))
+print(round(89.47*deg, 4))
